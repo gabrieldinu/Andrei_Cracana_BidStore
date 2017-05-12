1fcfbce01b172795efb611d453fcdb27dea4fd55
andrei.cracana <andrei.cracana@WS-IS-U3-3377.ad.fortech.ro>
2017-05-12 18:41:30 +0300

    weekend placut

--- BidStore/BidStore-web/src/main/java/ro/fortech/BidStore/view/data/DataListView.java
+++ BidStore/BidStore-web/src/main/java/ro/fortech/BidStore/view/data/DataListView.java
@@ -10,6 +10,9 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+import javax.faces.component.UICommand;
+import javax.faces.event.ActionEvent;
+
 import ro.fortech.BidStore.domain.Item;
 import ro.fortech.BidStore.service.ItemService;
  
@@ -17,15 +20,25 @@ import ro.fortech.BidStore.service.ItemService;
 @SessionScoped
 public class DataListView implements Serializable {
 	
+	
+	//Data
     private List<Item> items;
-    
+    private int totalRows;
     private Item selectedItem;
-     
-    private List<Item> filteredItems;
-     
+    //Paging
+    private int firstRow;
+    private int rowsPerPage;
+    private int totalPages;
+    private int pageRange;
+    private Integer[] pages;
+    private int currentPage;
+    
+    //Sorting
+    private String sortField;
+    private boolean sortAscending;
+    
     private Map<String,String> sortBy;
-    String sortColumn;
-    boolean sortAsc;
+    String sortColumn;   
     String sortOrder;
     
     @ManagedProperty("#{itemService}")
@@ -33,19 +46,95 @@ public class DataListView implements Serializable {
  
     @PostConstruct
     public void init() {
+    	//get items
         items = service.createItems(50);
+        
+        //pagination
+        rowsPerPage = 10;
+        pageRange = 10;
+        
+        //sort
+        //sortField = "id";
+        sortAscending = true;
+        
+        //sort old
         sortBy = new HashMap<String,String>();
         for (Field field : Item.class.getDeclaredFields()) sortBy.put(field.getName(),field.getName());
-        sortAsc = true;
+        
         sortOrder = "ascending";
     }
-     
+    
+    //Paging options
+    public void pageFirst() {
+    	page(0);
+    }
+    
+    public void pageNext() {
+    	page(firstRow + rowsPerPage);
+    }
+    
+    public void pagePrevious() {
+        page(firstRow - rowsPerPage);
+    }
+
+    public void pageLast() {
+        page(totalRows - ((totalRows % rowsPerPage != 0) ? totalRows % rowsPerPage : rowsPerPage));
+    }
+    
+    public void page(ActionEvent event) {
+        page(((Integer) ((UICommand) event.getComponent()).getValue() - 1) * rowsPerPage);
+    }
+
+    private void page(int firstRow) {
+        this.firstRow = firstRow;
+        loadDataList(); // Load requested page.
+    }
+    
+    //Sort actions
+    public void sort(ActionEvent event) {
+        String sortFieldAttribute = (String) event.getComponent().getAttributes().get("sortField");
+
+        // If the same field is sorted, then reverse order, else sort the new field ascending.
+        if (sortField.equals(sortFieldAttribute)) {
+            sortAscending = !sortAscending;
+        } else {
+            sortField = sortFieldAttribute;
+            sortAscending = true;
+        }
+
+        pageFirst(); // Go to first page and load requested page.
+    }
+    
+    //Loader
+    private void loadDataList() {
+
+        // Load list and totalCount.
+//        try {
+//            dataList = dao.list(firstRow, rowsPerPage, sortField, sortAscending);
+//            totalRows = dao.count();
+//        } catch (DAOException e) {
+//            throw new RuntimeException(e); // Handle it yourself.
+//        }
+    	
+
+        // Set currentPage, totalPages and pages.
+        currentPage = (totalRows / rowsPerPage) - ((totalRows - firstRow) / rowsPerPage) + 1;
+        totalPages = (totalRows / rowsPerPage) + ((totalRows % rowsPerPage != 0) ? 1 : 0);
+        int pagesLength = Math.min(pageRange, totalPages);
+        pages = new Integer[pagesLength];
+
+        // firstPage must be greater than 0 and lesser than totalPages-pageLength.
+        int firstPage = Math.min(Math.max(0, currentPage - (pageRange / 2)), totalPages - pagesLength);
+
+        // Create pages (page numbers for page links).
+        for (int i = 0; i < pagesLength; i++) {
+            pages[i] = ++firstPage;
+        }
+    }
+    
+    //Getters
     public List<Item> getItems() {
         return items;
-    }
- 
-    public List<Item> getFilteredItems() {
-        return filteredItems;
     }
  
     public Item getSelectedItem() {
@@ -56,14 +145,41 @@ public class DataListView implements Serializable {
         this.selectedItem = selectedItem;
     }
  
-    public void setFilteredItems(List<Item> filteredItems) {
-        this.filteredItems = filteredItems;
-    }
- 
     public void setService(ItemService service) {
         this.service = service;
     }
+    
+    public int getTotalRows() {
+        return totalRows;
+    }
 
+    public int getFirstRow() {
+        return firstRow;
+    }
+
+    public int getRowsPerPage() {
+        return rowsPerPage;
+    }
+
+    public Integer[] getPages() {
+        return pages;
+    }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    // Setters ------------------------------------------------------------------------------------
+
+    public void setRowsPerPage(int rowsPerPage) {
+        this.rowsPerPage = rowsPerPage;
+    }
+    
+    ////////
 	public Map<String, String> getSortBy() {
 		return sortBy;
 	}
@@ -81,14 +197,14 @@ public class DataListView implements Serializable {
 	}
 
 	public boolean isSortAsc() {
-		return sortAsc;
+		return sortAscending;
 	}
 
 	public void setSortAsc(boolean sortAsc) {
-		this.sortAsc = sortAsc;
+		this.sortAscending = sortAsc;
 	}
 	public void changeSortOrder(){
-		sortOrder = sortAsc ? "ascending" : "descending";
+		sortOrder = sortAscending ? "ascending" : "descending";
 	}
 
 	public String getSortOrder() {
